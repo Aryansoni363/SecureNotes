e4ad29aa99b217097101f91c4c722ec4efcd1dae
Aryansni363 <Aryansni363@gamil.com>
2025-06-28 13:27:27 +0530

    feat: add delete note route to backend

--- server/src/controllers/noteController.js
+++ server/src/controllers/noteController.js
@@ -18,3 +18,34 @@ export const getNotes = async (req, res) => {
     res.status(500).json({ message: 'Failed to fetch notes' });
   }
 };
+export const deleteNote = async (req, res) => {
+  try {
+    const note = await Note.findOneAndDelete({
+      _id: req.params.id,
+      userId: req.userId,
+    });
+
+    if (!note) return res.status(404).json({ message: "Note not found" });
+
+    res.status(200).json({ message: "Note deleted successfully" });
+  } catch (err) {
+    res.status(500).json({ message: "Failed to delete note" });
+  }
+};
+
+export const updateNote = async (req, res) => {
+  try {
+    const { title, content } = req.body;
+    const note = await Note.findOneAndUpdate(
+      { _id: req.params.id, userId: req.userId },
+      { title, content },
+      { new: true }
+    );
+
+    if (!note) return res.status(404).json({ message: "Note not found" });
+
+    res.status(200).json(note);
+  } catch (err) {
+    res.status(500).json({ message: "Failed to update note" });
+  }
+};
--- server/src/routes/noteRoutes.js
+++ server/src/routes/noteRoutes.js
@@ -1,10 +1,12 @@
 import express from 'express';
-import { createNote, getNotes } from '../controllers/noteController.js';
+import { createNote, getNotes , deleteNote , updateNote} from '../controllers/noteController.js';
 import { protect } from '../middlewares/authMiddleware.js';
 
 const router = express.Router();
 
 router.post('/', protect, createNote); // create a note
+router.delete('/:id', protect, deleteNote); // delete a note
+router.put('/:id', protect, updateNote); // update a note
 router.get('/', protect, getNotes);    // get all notes of logged-in user
 
 export default router;
